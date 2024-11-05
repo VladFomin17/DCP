3d58bbc37cf13762e8b59fd16a1b229ec523c1d5
VladFomin17 <fominvladosik@gmail.com>
2024-11-05 16:21:11 +0300

    добавил задачу Pair

---
+++ Pair/Pair/Pair.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pair
+{
+    public class Pair<T, U>
+    {
+        public T FirstValue;
+        public U SecondValue;
+
+        public Pair(T firstValue, U secondValue)
+        {
+            FirstValue = firstValue;
+            SecondValue = secondValue;
+        }
+
+        public T GetFirstValue() { return FirstValue; }
+        public U GetSecondValue() { return SecondValue; }
+
+        public void ChangeFirstValue(T firstValue) => FirstValue = firstValue;
+
+        public void ChangeSecondValue(U secondValue) => SecondValue = secondValue;
+    }
+}
---
+++ Pair/Pair/Program.cs
@@ -0,0 +1,20 @@
+﻿using Pair;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pair<int, string> FirstPair = new Pair<int, string>(11, "минут");
+
+        Pair<string, double> SecondPair = new Pair<string, double>("одна пятая", 0.2);
+
+        Console.WriteLine($"Значения в первом случае: {FirstPair.GetFirstValue()} и {FirstPair.GetSecondValue()}");
+        Console.WriteLine($"Значения во втором случае: {SecondPair.GetFirstValue()} и {SecondPair.GetSecondValue()}");
+
+        FirstPair.ChangeFirstValue(21);
+        FirstPair.ChangeSecondValue("minutes");
+        Console.WriteLine($"Измененные значения: {FirstPair.GetFirstValue()} и {FirstPair.GetSecondValue()}");
+
+    }
+}
